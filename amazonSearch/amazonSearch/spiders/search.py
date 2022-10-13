8db8b21e04fb14834d770c15a80b0aee95c79743
ArvinVaziri <109656096+ArvinVazirii@users.noreply.github.com>
2022-10-13 14:36:26 +0330

    The Last One

--- amazonSearch/amazonSearch/spiders/search.py
+++ amazonSearch/amazonSearch/spiders/search.py
@@ -6,28 +6,18 @@ class search(scrapy.Spider):
     name = 'search'
 
     # site_url = 'https://www.amazon.com/'
-    # searchurl = 's?k='
-    # specifyurl = '&i='
 
     urlinfos = {}
     searchfor = ''
 
     def start_requests(self):
 
-        # name = input('what you want to search for :')
-        # with open('categories.txt', 'r') as file:
-        #     list = json.load(file)
-
         with open('infos.txt', 'r') as file:
             self.urlinfos = json.load(file)
 
-        # with open('log.txt', 'r') as file:
-        #     self.search = json.load(file)
-
 
         yield scrapy.Request(list(self.urlinfos.items())[0][0], callback = self.parse)
-            # time.sleep(2)
-
+            
     def parse(self, response):
 
         namelist = response.css('.a-size-base-plus::text').getall()
